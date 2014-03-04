b3a9584f1a14ea32e6f087c866728f99746b1173
Trung Tran <trung.tran@willowtreeapps.com>
2014-03-04 14:42:30 -0500

    update app to include version number and start on login option

--- BathroomTree/BTAppDelegate.m
+++ BathroomTree/BTAppDelegate.m
@@ -16,6 +16,7 @@
 @property (nonatomic, readwrite, strong) NSStatusItem *statusItem;
 @property (nonatomic, readwrite, strong) IBOutlet BTStatusItemView *statusItemView;
 @property (nonatomic, readwrite, strong) IBOutlet NSMenu *menu;
+@property (nonatomic, readwrite, strong) IBOutlet NSMenuItem *versionItem;
 @property (nonatomic, readwrite, strong) IBOutlet NSMenuItem *notify1Item;
 @property (nonatomic, readwrite, strong) IBOutlet NSMenuItem *notify2Item;
 @property (nonatomic, readwrite, strong) IBOutlet NSMenuItem *notify3Item;
@@ -27,6 +28,7 @@
 @property (nonatomic, readwrite, strong) IBOutlet NSMenuItem *poll5Item;
 @property (nonatomic, readwrite, strong) IBOutlet NSMenuItem *poll15Item;
 @property (nonatomic, readwrite, strong) IBOutlet NSMenuItem *poll30Item;
+@property (nonatomic, readwrite, strong) IBOutlet NSMenuItem *startLoginItem;
 @property (nonatomic, readwrite, strong) NSArray *pollItems;
 
 @end
@@ -39,6 +41,8 @@
     [self setupPollItems];
     [self setupStatusItem];
     [self setupMenuItemActions];
+    [self setupLoginItemAction];
+    [self setupVersionItem];
     
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(bathroomManagerDidUpdateStateNotification:)
@@ -53,6 +57,42 @@
     [[BTBathroomManager defaultManager] stopPolling];
     [[NSNotificationCenter defaultCenter] removeObserver:self];
     [[NSUserDefaults standardUserDefaults] synchronize];
+}
+
+- (void)setupVersionItem
+{
+    NSString *version = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
+    [[self versionItem] setTitle:version];
+}
+
+- (void)setupLoginItemAction
+{
+    [[self startLoginItem] setTarget:self];
+    [[self startLoginItem] setAction:@selector(toggleStartLoginItem:)];
+    
+    if ([self appIsLoginItem])
+    {
+        [[self startLoginItem] setState:NSOnState];
+    }
+}
+
+- (void)toggleStartLoginItem:(NSMenuItem *)item
+{
+    switch ((NSCellStateValue)[item state])
+    {
+        case NSOffState:
+            [self addAppAsLoginItem];
+            [item setState:NSOnState];
+            break;
+            
+        case NSOnState:
+            [self deleteAppFromLoginItem];
+            [item setState:NSOffState];
+            break;
+            
+        default:
+            break;
+    }
 }
 
 - (void)bathroomManagerDidUpdateStateNotification:(NSNotification *)notification
@@ -162,6 +202,110 @@
     {
         [menuItem setState:NSOffState];
     }
+}
+
+- (void)addAppAsLoginItem
+{
+	NSString * appPath = [[NSBundle mainBundle] bundlePath];
+    
+	// This will retrieve the path for the application
+	// For example, /Applications/test.app
+	CFURLRef url = (__bridge CFURLRef)[NSURL fileURLWithPath:appPath];
+    
+	// Create a reference to the shared file list.
+    // We are adding it to the current user only.
+    // If we want to add it all users, use
+    // kLSSharedFileListGlobalLoginItems instead of
+    //kLSSharedFileListSessionLoginItems
+	LSSharedFileListRef loginItems = LSSharedFileListCreate(NULL,
+                                                            kLSSharedFileListSessionLoginItems, NULL);
+	if (loginItems)
+    {
+		//Insert an item to the list.
+		LSSharedFileListItemRef item = LSSharedFileListInsertItemURL(loginItems,
+                                                                     kLSSharedFileListItemLast, NULL, NULL,
+                                                                     url, NULL, NULL);
+		if (item)
+        {
+			CFRelease(item);
+        }
+	}
+    
+	CFRelease(loginItems);
+}
+
+- (BOOL)appIsLoginItem
+{
+	NSString * appPath = [[NSBundle mainBundle] bundlePath];
+    
+	// This will retrieve the path for the application
+	// For example, /Applications/test.app
+	CFURLRef url = (__bridge CFURLRef)[NSURL fileURLWithPath:appPath];
+    
+	// Create a reference to the shared file list.
+	LSSharedFileListRef loginItems = LSSharedFileListCreate(NULL,
+                                                            kLSSharedFileListSessionLoginItems,
+                                                            NULL);
+    
+	if (loginItems)
+    {
+		UInt32 seedValue;
+		//Retrieve the list of Login Items and cast them to
+		// a NSArray so that it will be easier to iterate.
+		NSArray *loginItemsArray = (__bridge NSArray *)LSSharedFileListCopySnapshot(loginItems, &seedValue);
+		for(int i = 0; i < [loginItemsArray count]; i++)
+        {
+			LSSharedFileListItemRef itemRef = (__bridge LSSharedFileListItemRef)[loginItemsArray
+                                                                                 objectAtIndex:i];
+			//Resolve the item with URL
+			if (LSSharedFileListItemResolve(itemRef, 0, (CFURLRef*) &url, NULL) == noErr)
+            {
+				NSString * urlPath = [(__bridge NSURL*)url path];
+				if ([urlPath compare:appPath] == NSOrderedSame)
+                {
+                    return YES;
+				}
+			}
+		}
+	}
+    
+    return NO;
+}
+
+- (void)deleteAppFromLoginItem
+{
+	NSString * appPath = [[NSBundle mainBundle] bundlePath];
+    
+	// This will retrieve the path for the application
+	// For example, /Applications/test.app
+	CFURLRef url = (__bridge CFURLRef)[NSURL fileURLWithPath:appPath];
+    
+	// Create a reference to the shared file list.
+	LSSharedFileListRef loginItems = LSSharedFileListCreate(NULL,
+                                                            kLSSharedFileListSessionLoginItems,
+                                                            NULL);
+    
+	if (loginItems)
+    {
+		UInt32 seedValue;
+		//Retrieve the list of Login Items and cast them to
+		// a NSArray so that it will be easier to iterate.
+		NSArray *loginItemsArray = (__bridge NSArray *)LSSharedFileListCopySnapshot(loginItems, &seedValue);
+		for(int i = 0; i < [loginItemsArray count]; i++)
+        {
+			LSSharedFileListItemRef itemRef = (__bridge LSSharedFileListItemRef)[loginItemsArray
+                                                                        objectAtIndex:i];
+			//Resolve the item with URL
+			if (LSSharedFileListItemResolve(itemRef, 0, (CFURLRef*) &url, NULL) == noErr)
+            {
+				NSString * urlPath = [(__bridge NSURL*)url path];
+				if ([urlPath compare:appPath] == NSOrderedSame)
+                {
+					LSSharedFileListItemRemove(loginItems,itemRef);
+				}
+			}
+		}
+	}
 }
 
 @end
